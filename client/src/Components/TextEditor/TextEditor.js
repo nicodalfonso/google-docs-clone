963404b39e3fcf7021ac1ae9bad354a021208b3a
Nico Alfonso <nicodalfonso@gmail.com>
2022-01-24 08:59:02 -0500

    Adds socket connection between client and server

--- client/src/Components/TextEditor/TextEditor.js
+++ client/src/Components/TextEditor/TextEditor.js
@@ -1,6 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -15,6 +16,13 @@ const TOOLBAR_OPTIONS = [
 ];
 
 export const TextEditor = () => {
+    useEffect(() => {
+        const socket = io("http://localhost:3001");
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return;
 
